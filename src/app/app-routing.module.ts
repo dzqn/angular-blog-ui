4d5d53cb7ac89ee34a2b2f2c1236c2cfabf92a0e
Düzgün Tutar <duzguntutar@gmail.com>
2019-10-16 11:09:05 +0300

    children route changed.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,11 +28,17 @@ const routes: Routes = [
   },
   {
     path: "admin", component: AdminlayoutComponent, children: [
-      { path: "home", component: AdminhomeComponent },
-      { path: "category", component: CategoryComponent },
-      { path: "category/add", component: CategoryAddComponent },
-      { path: "category/list", component: CategoryListComponent },
-      { path: "post", component: PostComponent }
+      {
+        path: "home", component: AdminhomeComponent,children:[{
+          path:"post",component:PostComponent
+        }]
+      },
+      {
+        path: "category", component: CategoryComponent
+      },
+      {
+        path: "post", component: PostComponent
+      }
     ]
   }
 ];
