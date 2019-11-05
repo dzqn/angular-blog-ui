06d3e5c26d5e0f60d0c42f41599485bc0d4aa05b
Düzgün Tutar <duzguntutar@gmail.com>
2019-11-05 23:01:13 +0300

    html changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ import { AdminLayoutComponent } from './admin/component/adminlayout/adminlayout.
 import { AdminHomeComponent } from './admin/page/adminhome/adminhome.component';
 import { AdminCategoryComponent } from './admin/page/admincategory/admincategory.component';
 import { PostComponent } from './admin/page/post/post.component';
-import {AdminCategoryAddComponent} from './admin/component/admin-category-add/admin-category-add.component';
-import {AdminCategoryListComponent} from './admin/component/admin-category-list/admin-category-list.component';
+import { AdminCategoryAddComponent } from './admin/component/admin-category-add/admin-category-add.component';
+import { AdminCategoryListComponent } from './admin/component/admin-category-list/admin-category-list.component';
 
 
 
@@ -31,19 +31,21 @@ const routes: Routes = [
   {
     path: "admin", component: AdminLayoutComponent, children: [
       {
-        path: "home", component: AdminHomeComponent, children: [{
-          path: "post", component: PostComponent
-        }]
+        path: "home", component: AdminHomeComponent, children: [
+          {
+            path: "post", component: PostComponent
+          }
+        ]
       },
       {
-        path: "category", component: AdminCategoryComponent, children:[
+        path: "category", component: AdminCategoryComponent, children: [
           {
-          path:"addcategory", component:AdminCategoryAddComponent
-        },
-        {
-          path:"listcategory", component:AdminCategoryListComponent
-        }
-      ]
+            path: "addcategory", component: AdminCategoryAddComponent
+          },
+          {
+            path: "listcategory", component: AdminCategoryListComponent
+          }
+        ]
       },
       {
         path: "post", component: PostComponent
